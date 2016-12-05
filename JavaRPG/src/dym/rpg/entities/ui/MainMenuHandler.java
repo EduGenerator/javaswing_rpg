d7feca8837d4a4281dad8654fe442df451a8420c
EduGenerator <susanbrodt@10.110.68.98>
2016-12-06 00:35:45 +0100

    Oh god help

---
+++ JavaRPG/src/dym/rpg/entities/ui/MainMenuHandler.java
@@ -0,0 +1,15 @@
+package dym.rpg.entities.ui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
+import dym.rpg.Display;
+import dym.rpg.Input;
+import dym.rpg.entities.Entity;
+import dym.rpg.scene.SceneManager;
+
+public class MainMenuHandler extends Entity {
+
+}
---
+++ JavaRPG/src/dym/rpg/scene/staticScenes/MainMenu.java
@@ -0,0 +1,11 @@
+package dym.rpg.scene.staticScenes;
+
+import dym.rpg.entities.ui.MainMenuHandler;
+import dym.rpg.scene.Scene;
+
+public class MainMenu extends Scene {
+	public MainMenu(){
+		super(true);
+		//this.entities.add(newMainMenuHandler);
+		}
+}
