e255167b960b4a61a29e9ebdb157f3a625aff891
EduGenerator <susanbrodt@10.110.68.98>
2016-12-06 19:42:28 +0100

    sdfghjk

--- JavaRPG/src/dym/rpg/entities/ui/MainMenuHandler.java
+++ JavaRPG/src/dym/rpg/entities/ui/MainMenuHandler.java
@@ -11,5 +11,36 @@ import dym.rpg.entities.Entity;
 import dym.rpg.scene.SceneManager;
 
 public class MainMenuHandler extends Entity {
-
+	public MainMenuHandler() {
+		super();
+		
+	}
+	@Override
+	public void update() {
+		if (Input.keysDown.contains(KeyEvent.VK_LEFT)) {
+		
+			Input.keysDown.remove((Object)KeyEvent.VK_LEFT);
+		}
+		if (Input.keysDown.contains(KeyEvent.VK_RIGHT)) {
+			
+			Input.keysDown.remove((Object)KeyEvent.VK_RIGHT);
+		}
+		if (Input.keysDown.contains(KeyEvent.VK_UP)) {
+			
+			
+			
+			Input.keysDown.remove((Object)KeyEvent.VK_UP);
+		}
+		if (Input.keysDown.contains(KeyEvent.VK_DOWN)) {
+			
+			
+			
+			Input.keysDown.remove((Object)KeyEvent.VK_DOWN);
+		}
+	}
+	
+	@Override
+	public void draw(Graphics g) {
+		
+	}
 }
--- JavaRPG/src/dym/rpg/scene/SceneManager.java
+++ JavaRPG/src/dym/rpg/scene/SceneManager.java
@@ -1,5 +1,6 @@
 package dym.rpg.scene;
 
+import dym.rpg.scene.staticScenes.MainMenu;
 import dym.rpg.scene.staticScenes.SettingsScreen;
 import dym.rpg.scene.staticScenes.TestScene;
 import dym.rpg.scene.staticScenes.TestScene2;
@@ -8,6 +9,7 @@ public class SceneManager {
 	public static TestScene testScene1 = new TestScene();
 	public static TestScene2 testScene2 = new TestScene2();
 	public static SettingsScreen settings = new SettingsScreen();
+	public static MainMenu mainMenu = new MainMenu();
 	
 	public static Scene currentScene = testScene1;
 	public static Scene prevScene = currentScene;
